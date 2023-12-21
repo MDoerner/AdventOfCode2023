9b4025de333c5dc1c28b0f8f822aac470a1f97b8
Max Doerner <maxdoerner@gmx.net>
2023-12-22 00:27:21 +0100

    Add solution for part 1 of day 21 of AdventOfCode 2023 in clojure
    I will come back for part 2 later.

--- Clojure/advent_of_code_2023/src/advent_of_code_2023/days/day21.clj
+++ Clojure/advent_of_code_2023/src/advent_of_code_2023/days/day21.clj
@@ -1,19 +1,148 @@
-(ns advent-of-code-2023.days.day21)
+(ns advent-of-code-2023.days.day21
+  (:require [clojure.string :as str]))
 
+(defn- parse_line_of_rocks
+  [y text]
+  (let [positions (str/split text #"")]
+    (->> positions
+         (map-indexed #(if (contains? #{"#" "S"} %2)
+                         (let [point {:x %1 :y y}
+                               type (case %2
+                                      "#" :rock
+                                      "S" :start)]
+                           {:point point :type type})))
+         (filter some?)
+         (reduce (fn [rocks_and_start {point :point type :type}]
+                   (case type
+                     :start (assoc rocks_and_start :start point)
+                     :rock (update rocks_and_start :rocks #(conj %1 point))))
+                 {:rocks #{}}))))
+
+(defn- parse_rocks
+  [lines]
+  (->> lines
+       (map-indexed parse_line_of_rocks)
+       (reduce (fn [rocks_and_start {new_rocks :rocks new_start :start}]
+                 (let [with_new_rocks (update rocks_and_start :rocks #(into %1 new_rocks))]
+                   (if (nil? new_start)
+                     with_new_rocks
+                     (assoc with_new_rocks :start new_start))))
+               {:rocks #{}})))
 
 (defn parse_input
   "Parses the input for day 21 of AdventOfCode 2023"
   [input_string]
-  [])
+  (let [lines (remove str/blank? (str/split input_string #"\r?\n"))
+        {rocks :rocks start :start} (parse_rocks lines)
+        height (count lines)
+        widths (count (first lines))]
+    {
+     :steps1 64
+     :steps2 26501365
+     :start start
+     :width widths
+     :height height
+     :rocks rocks
+     }
+    ))
+
+(defn- add_positions
+  [pos other_pos]
+  (merge-with + pos other_pos))
+
+(defn- in_bounds?
+  [width height point]
+  (let [{x :x y :y} point]
+    (and (>= x 0) (>= y 0) (< x width) (< y height))))
+
+(defn- move_once
+  [rocks width height point]
+  (->> [{:x 0 :y -1} {:x 0 :y 1} {:x 1 :y 0} {:x -1 :y 0}]
+       (map #(add_positions point %1))
+       (filter #(and (not (contains? rocks %1))
+                     (in_bounds? width height %1)))))
+
+(defn- move_twice
+  [rocks width height point]
+  (->> point
+       (move_once rocks width height)
+       (map #(move_once rocks width height %1))
+       (reduce into #{})))
+
+(defn- move
+  [rocks width height point n]
+  (if (= n 1)
+    (move_once rocks width height point)
+    (if (not= (mod n 2) 0)
+      (let [one_less_to_move (move_once rocks width height point)]
+        (->> one_less_to_move
+             (map #(move rocks width height %1 (dec n)))
+             (reduce into #{})))
+      (loop [reachable #{point} barely_reachable #{point} m (/ n 2)]
+        (if (= m 0)
+          reachable
+          (let [new_barely_reachable(->> barely_reachable
+                                   (map #(move_twice rocks width height %1))
+                                   (reduce into #{})
+                                   (filter #(not (contains? reachable %1)))
+                                   (into #{}))
+              new_reachable (into reachable new_barely_reachable)]
+            (recur new_reachable new_barely_reachable (dec m))))))))
+
 
 
 (defn solve_part1
   "Solves part 1 of day 21 of AdventOfCode 2023"
   [input]
-  "")
+  (let [{steps :steps1 start :start width :width height :height rocks :rocks} input]
+    (str (count (move rocks width height start steps)))))
 
+
+(defn- map_point
+  [width height point]
+  (if (in_bounds? width height point)
+    point
+    (let [{x :x y :y} point]
+      {:x (mod x width) :y (mod y height)})))
+
+(defn- move_once_looping
+  [rocks width height point]
+  (->> [{:x 0 :y -1} {:x 0 :y 1} {:x 1 :y 0} {:x -1 :y 0}]
+       (map #(add_positions point %1))
+       (filter #(not (contains? rocks (map_point width height %1))))))
+
+(defn- move_twice_looping
+  [rocks width height point]
+  (->> point
+       (move_once_looping rocks width height)
+       (map #(move_once_looping rocks width height %1))
+       (reduce into #{})))
+
+(defn- move_looping
+  [rocks width height point n]
+  (if (= n 1)
+    (move_once_looping rocks width height point)
+    (if (not= (mod n 2) 0)
+      (let [one_less_to_move (move_once_looping rocks width height point)]
+        (->> one_less_to_move
+             (map #(move_looping rocks width height %1 (dec n)))
+             (reduce into #{})))
+      (loop [reachable #{point} barely_reachable #{point} m (/ n 2)]
+        (if (= m 0)
+          reachable
+          (let [new_barely_reachable(->> barely_reachable
+                                         (map #(move_twice_looping rocks width height %1))
+                                         (reduce into #{})
+                                         (filter #(not (contains? reachable %1)))
+                                         (into #{}))
+                new_reachable (into reachable new_barely_reachable)]
+            (recur new_reachable new_barely_reachable (dec m))))))))
 
 (defn solve_part2
   "Solves part 2 of day 21 of AdventOfCode 2023"
   [input]
-  "")
+  (let [{steps :steps2 start :start width :width height :height rocks :rocks} input]
+    ;(println (move_twice rocks width height {:x 0 :y 4}))
+    ;(println (move_once_looping rocks width height {:x -1 :y 4}))
+    (str (count (move_looping rocks width height start steps)))
+    ))
---
+++ Clojure/advent_of_code_2023/test/advent_of_code_2023/days/day21_test.clj
@@ -0,0 +1,92 @@
+(ns advent-of-code-2023.days.day21-test
+  (:require [clojure.test :refer :all])
+  (:require [advent-of-code-2023.days.day21 :refer [parse_input solve_part1 solve_part2]])
+  (:require [advent-of-code-2023.core :refer [solve_problem]]))
+
+(def day 21)
+(def example_input_1 "...........\n.....###.#.\n.###.##..#.\n..#.#...#..\n....#.#....\n.##..S####.\n.##..#...#.\n.......##..\n.##.#.####.\n.##..##.##.\n...........\n")
+(def example_input_2 example_input_1)
+
+(deftest parse_input-test
+  (testing "Test parsing for example input"
+    (let [input_text example_input_1
+          parsed_input (parse_input input_text)
+          expected_input {
+                          :steps1 64
+                          :setps2 26501365
+                          :start {:x 5 :y 5}
+                          :width 11
+                          :height 11
+                          :rocks #{
+                                   {:x 5 :y 1}
+                                   {:x 6 :y 1}
+                                   {:x 7 :y 1}
+                                   {:x 9 :y 1}
+                                   {:x 1 :y 2}
+                                   {:x 2 :y 2}
+                                   {:x 3 :y 2}
+                                   {:x 5 :y 2}
+                                   {:x 6 :y 2}
+                                   {:x 9 :y 2}
+                                   {:x 2 :y 3}
+                                   {:x 4 :y 3}
+                                   {:x 8 :y 3}
+                                   {:x 4 :y 4}
+                                   {:x 6 :y 4}
+                                   {:x 1 :y 5}
+                                   {:x 2 :y 5}
+                                   {:x 6 :y 5}
+                                   {:x 7 :y 5}
+                                   {:x 8 :y 5}
+                                   {:x 9 :y 5}
+                                   {:x 1 :y 6}
+                                   {:x 2 :y 6}
+                                   {:x 5 :y 6}
+                                   {:x 9 :y 6}
+                                   {:x 7 :y 7}
+                                   {:x 8 :y 7}
+                                   {:x 1 :y 8}
+                                   {:x 2 :y 8}
+                                   {:x 4 :y 8}
+                                   {:x 6 :y 8}
+                                   {:x 7 :y 8}
+                                   {:x 8 :y 8}
+                                   {:x 9 :y 8}
+                                   {:x 1 :y 9}
+                                   {:x 2 :y 9}
+                                   {:x 5 :y 9}
+                                   {:x 6 :y 9}
+                                   {:x 8 :y 9}
+                                   {:x 9 :y 9}
+                                  }
+                          }]
+      (is (= parsed_input expected_input)))))
+
+(deftest solve_part1-example1-test
+  (testing "Test for example input 1 to part 1"
+    (let [input_text example_input_1
+          parsed_input (parse_input input_text)
+          adjusted_input (assoc parsed_input :steps1 6)
+          result (solve_part1 adjusted_input)]
+      (is (= result "16")))))
+
+(deftest solve_part2-example2-test
+  (testing "Test for example input 2 to part 2"
+    (let [input_text example_input_2
+          parsed_input (parse_input input_text)
+          adjusted_inputs (into [] (map #(assoc parsed_input :steps2 %1) [6 10 50 100 500 1000 5000]))
+          results (into [] (map #(time (solve_part2 %1)) adjusted_inputs))
+          expecteds ["16" "50" "1594" "6536" "167004" "668697" "16733044"]]
+      (is (= results expecteds)))))
+
+
+
+(deftest solve_part1-input-test
+  (testing "Test for real input to part 1"
+    (is (= (solve_problem day 1) "3724"))))
+
+
+(deftest solve_part2-input-test
+  (testing "Test for real input to part 2"
+    (is (= (solve_problem day 2) "243081086866483"))))
+
